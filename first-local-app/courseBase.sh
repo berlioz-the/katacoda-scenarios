7ee092b9effd45d88e3652a5a02c825501fe63d1
Ruben Hakopian <ruben.hakopian@gmail.com>
2019-06-25 15:24:56 -0700

    improved environment setup.

--- first-local-app/courseBase.sh
+++ first-local-app/courseBase.sh
@@ -1,5 +1,12 @@
 echo "********************************"
-echo "********************************"
-echo "******** COURSE BASE ***********"
-echo "********************************"
-echo "********************************"
+echo "*******************************"
+echo 'export BERLIOZ_DOCKER_HOST=docker' >> ~/.bashrc
+
+echo "***** INSTALLING NODE.JS ******"
+curl -sL https://deb.nodesource.com/setup_10.x | bash -
+apt-get install -y nodejs build-essential
+
+echo "***** INSTALLING BERLIOZ ******"
+npm install berlioz -g --unsafe-perm
+
+echo "**** ENVIRONMENT IS READY! ****"

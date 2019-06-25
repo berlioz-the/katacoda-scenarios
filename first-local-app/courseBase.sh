88af64d5ce9b7fbd534a54ac19828d24ae53ef75
Ruben Hakopian <ruben.hakopian@gmail.com>
2019-06-25 15:29:17 -0700

    moved init script

--- first-local-app/courseBase.sh
+++ first-local-app/courseBase.sh
@@ -1,12 +1,11 @@
-echo "********************************"
-echo "*******************************"
-echo 'export BERLIOZ_DOCKER_HOST=docker' >> ~/.bashrc
+# echo "*******************************"
+# echo 'export BERLIOZ_DOCKER_HOST=docker' >> ~/.bashrc
 
-echo "***** INSTALLING NODE.JS ******"
-curl -sL https://deb.nodesource.com/setup_10.x | bash -
-apt-get install -y nodejs build-essential
+# echo "***** INSTALLING NODE.JS ******"
+# curl -sL https://deb.nodesource.com/setup_10.x | bash -
+# apt-get install -y nodejs build-essential
 
-echo "***** INSTALLING BERLIOZ ******"
-npm install berlioz -g --unsafe-perm
+# echo "***** INSTALLING BERLIOZ ******"
+# npm install berlioz -g --unsafe-perm
 
-echo "**** ENVIRONMENT IS READY! ****"
+# echo "**** ENVIRONMENT IS READY! ****"

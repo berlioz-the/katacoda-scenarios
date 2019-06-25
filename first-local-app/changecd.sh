ee411b56da8aeefc2fed036de3603c934c73b49b
Ruben Hakopian <ruben.hakopian@gmail.com>
2019-06-25 15:42:37 -0700

    environment setup

--- first-local-app/changecd.sh
+++ first-local-app/changecd.sh
@@ -1,9 +1,10 @@
 # echo "*******************************"
 # echo 'export BERLIOZ_DOCKER_HOST=docker' >> ~/.bashrc
-echo "***** DOWNLOADING NODE.JS ******"
-curl -sL https://deb.nodesource.com/setup_10.x | bash -
-echo "****** INSTALLING NODE.JS ******"
-apt-get install -y nodejs build-essential
-echo "***** INSTALLING BERLIOZ ******"
-npm install berlioz -g --unsafe-perm
-echo "**** ENVIRONMENT IS READY! ****"
+# echo "***** DOWNLOADING NODE.JS ******"
+echo "***** BERLIOZ ENVIRONEMNT SETUP ******"
+(curl -sL https://deb.nodesource.com/setup_10.x | bash -) ; (apt-get install -y nodejs build-essential) ; (npm install berlioz -g --unsafe-perm)
+# echo "****** INSTALLING NODE.JS ******"
+# apt-get install -y nodejs build-essential
+# echo "***** INSTALLING BERLIOZ ******"
+# npm install berlioz -g --unsafe-perm
+# echo "**** ENVIRONMENT IS READY! ****"
--- first-local-app/step0.md
+++ first-local-app/step0.md
@@ -1,8 +1,6 @@
-We are setting up Katacode environment wiht Berlioz tools and dependencies. This process would take about a minute.  
-
-This step is required only for running example in Katacoda. If you follow steps on your computer, you can skip this. The following command would install Node.js and Berlioz CLI tool.
+We are setting up Katacode environment with Berlioz tools and dependencies. This process would take about a minute.  
 
 `export BERLIOZ_DOCKER_HOST=docker; (npm install berlioz -g --unsafe-perm)`{{execute}}
 
-For setting up environment on your computer follow scenarios:
+While this is happening you can also setup the environment on your computer by following the scenarios:
 * MacOS: https://www.katacoda.com/berlioz/scenarios/install-on-mac
